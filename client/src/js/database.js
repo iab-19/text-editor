f0382ca04cb96038a1d38014a26b5c213acd9df8
Ibrahima <iab.jr10@gmail.com>
2023-09-28 05:57:39 -0400

    Add functions to update and display text in the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,64 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+// create a new database called 'jate' and user version 1
+  openDB('finesse', 1, {
+    // Add database schea if it has not already been initialized
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('finesse')) {
+        console.log('finesse database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('finesse', { keyPath: 'id', autoIncrement: true });
+      console.log('finesse database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // console.error('putDb not implemented');
+  console.log('PUT to the database');
+
+  // Create a connection to the database and version we want to use
+  const finesseDb = await openDB('finesse', 1);
+
+  // Create a new transaction and specify the database and data privileges
+  const tx = finesseDb.transaction('finesse', 'readwrite');
+
+  // Open the desired object store
+  const store = tx.objectStore('finesse');
+
+  // Use the .put method to update the data in the database
+  const request = store.put({ text: content });
+
+  // Get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // console.error('getDb not implemented');
+  console.log('GET from the database');
 
+  // Create a connection to the database and version we want to use
+  const finesseDb = await openDB('finesse', 1);
+
+  // Create a new transaction and specify the database and data privileges
+  const tx = finesseDb.transaction('finesse', 'readonly');
+
+  // Open the desired object store
+  const store = tx.objectStore('finesse');
+
+  // Use the .getAll method to get all data in the database
+  const request = store.getAll();
+
+  // Get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 initdb();
